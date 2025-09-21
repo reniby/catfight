29c24293ed17321f3396233a3a77523f6850e76b
Emma Waddell <52298111+reniby@users.noreply.github.com>
2025-09-20 20:07:43 -0400

    top down

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,22 +3,22 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -800.0
 
-@onready var camera = $"../Camera2D"
 @export var player: int
 @export var color: String
+@onready var death_timer: Timer = $DeathTimer
+@onready var camera = $"../Camera2D"
+@onready var p2: CharacterBody2D = $"../Player2"
 
 var trail
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var count = 0
 func _ready():
 	trail = get_node("Trail")
 	trail.default_color = color
 
 func _physics_process(delta):
-	check_intersections()
-
 	var left = camera.get_viewport_rect().size.x/2 * -1
 	var right = camera.get_viewport_rect().size.x/2
 	var top = camera.get_viewport_rect().size.y/2 * -1
@@ -33,47 +33,27 @@ func _physics_process(delta):
 	elif position.x < left - 10:
 		position.x = right
 
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
 	if player == 1:
-		if Input.is_action_just_pressed("jump_p1") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
-		var direction = Input.get_axis("ui_left", "ui_right")
-		if direction:
-			velocity.x = direction * SPEED
+		var y_direction = Input.get_axis("up_p1", "down_p1")
+		var x_direction = Input.get_axis("left_p1", "right_p1")
+		if y_direction:
+			velocity.y = y_direction * SPEED
+		else:
+			velocity.y = move_toward(velocity.y, 0, SPEED)
+		if x_direction:
+			velocity.x = x_direction * SPEED
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 	if player == 2:
-		if Input.is_action_just_pressed("jump_p2") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
-		var direction = Input.get_axis("left_p2", "right_p2")
-		if direction:
-			velocity.x = direction * SPEED
+		var y_direction = Input.get_axis("up_p2", "down_p2")
+		var x_direction = Input.get_axis("left_p2", "right_p2")
+		if y_direction:
+			velocity.y = y_direction * SPEED
+		else:
+			velocity.y = move_toward(velocity.y, 0, SPEED)
+		if x_direction:
+			velocity.x = x_direction * SPEED
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
+
 	move_and_slide()
-
-func check_intersections():
-	for i in range(1, trail.points.size()):
-		var a1 = trail.points[i - 1]
-		var a2 = trail.points[i]
-		
-		for j in range(1, trail.points.size()):
-			if abs(i - j) <= 3:
-				continue
-			
-			var b1 = trail.points[j - 1]
-			var b2 = trail.points[j]
-
-			if Geometry2D.segment_intersects_segment(a1, a2, b1, b2) != null:
-				check_betwixt(i-1,j)
-
-func check_betwixt(start,end):
-	#var polygon = Polygon2D.new()
-	var shape = PackedVector2Array(trail.points.slice(min(start,end),max(start,end)+1))
-	#polygon.set_polygon(shape)
-	var p2 = $"../Player2"
-	if Geometry2D.is_point_in_polygon(p2.global_position/p2.global_scale, shape):
-		print("DEATH")
--- scripts/trail.gd
+++ scripts/trail.gd
@@ -1,26 +1,41 @@
 extends Line2D
 
 @export var length = 200
+@onready var body = $Area2D
+@onready var shapes = []
 var point = Vector2()
 var prev = null
 var active = true
 
+func _ready():
+	remove_point(0)
+	#if self.get_parent().player == 1:
+		#body.collision_layer = 3
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if active:
+	if get_parent().velocity != Vector2(0,0):
 		global_position = Vector2(0,0)
 		global_rotation = 0
 		
 		point = get_parent().global_position/get_parent().scale
 		add_point(point)
-		#add_point(point-Vector2(0.1,0.1))
-		#add_point(point-Vector2(0.2,0.2))
-	else:
+		if len(points) > 0:
+			var shape = CollisionShape2D.new()
+			$Area2D.add_child(shape)
+			var segment = SegmentShape2D.new()
+			segment.a = points[points.size()-1]
+			segment.b = points[points.size()-2]
+			shape.shape = segment
+			shapes.append(shape)
+	elif get_point_count() > 0:
+		shapes.pop_at(0).queue_free()
 		remove_point(0)
 
-	while get_point_count() > length:
+	while get_point_count() >= length:
+		shapes.pop_at(0).queue_free()
 		remove_point(0)
-	
-	if get_point_count() == 0:
-		queue_free()
-	
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+		if get_parent().player != body.player:
+			print(get_parent().player, " inbody ", body.player)
