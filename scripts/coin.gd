7d46db4b6f97d5c242ba3c454655c6e0776cf041
Emma Waddell <52298111+reniby@users.noreply.github.com>
2025-10-20 22:01:59 -0400

    fix coin overlap

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -3,26 +3,28 @@ extends Node2D
 var max_y = 594
 var max_x = 1086
 var rng = RandomNumberGenerator.new()
-# Called when the node enters the scene tree for the first time.
+@onready var area: Area2D = $Area2D
+@onready var sprite: AnimatedSprite2D = $Sprite2D
+
 func _ready() -> void:
 	choose_location()
 
-	
-	
+func _physics_process(delta: float) -> void:
+	var overlapping = false
 
+	if len(area.get_overlapping_bodies()):
+		overlapping = true
+		choose_location()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	if not overlapping:
+		call_deferred("show")
 	
 func choose_location():
+	hide()
 	var x = rng.randf_range(-max_x / 2, max_x / 2)
 	var y = rng.randf_range(-max_y / 2, max_y / 2)
 	position = Vector2(x, y)
 
-	
-	
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
 		body.score += 1
-		choose_location()
