0b887fa341dd4839d5f9ab460fb8ffefaa35e119
itaibdr <itaicarl@gmail.com>
2025-10-26 21:49:27 -0400

    font and coin particles fix

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -10,14 +10,15 @@ var rng = RandomNumberGenerator.new()
 
 func _ready() -> void:
 	choose_location()
-	
 
 func _physics_process(_delta: float) -> void:
 	var overlapping = false
 
 	for body in area.get_overlapping_bodies():
 		hide()
+		
 		particles.emitting = false
+		particles.restart()
 		overlapping = true
 		choose_location()
 		if body is CharacterBody2D:
@@ -30,19 +31,15 @@ func _physics_process(_delta: float) -> void:
 		sprite.scale = Vector2(0,0)
 		sprite.rotation = 0
 		var tween = get_tree().create_tween()
-		tween.tween_property(sprite, "rotation", 2*PI, 0.3)
+		tween.tween_property(sprite, "rotation", 8*PI, 0.3)
 		tween.set_parallel()
 		tween.tween_property(sprite, "scale", Vector2(1,1), 0.3)
-		call_deferred("emit")
-		#tween.tween_callback(sprite.queue_free)
-	
+		
+
 func choose_location():
 	var x = rng.randf_range(-max_x / 2, max_x / 2)
 	var y = rng.randf_range(-max_y / 2, max_y / 2)
 	position = Vector2(x, y)
-	
+
 func emit():
 	particles.emitting = true
-
-	
-	
