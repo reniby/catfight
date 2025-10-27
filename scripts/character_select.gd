760383b2566d13e9d098cd230d034a163d88d333
itaibdr <itaicarl@gmail.com>
2025-10-26 20:22:32 -0400

    tilemap and screenshake

--- scripts/character_select.gd
+++ scripts/character_select.gd
@@ -1,7 +1,15 @@
 extends Node2D
 @onready var labels: Array[Label] = [$Label,$Label2,$Label3,$Label4]
 @onready var press_play: Label = $PressPlay
+@onready var camera_2d: Camera2D = $Camera2D
 
+var rng = RandomNumberGenerator.new()
+var shake_strength = 0.0
+var randomStrength = 30.0
+var shakeFade = 5.0
+
+func apply_shake():
+	shake_strength = randomStrength
 var character_input = [{
 	"up": "up_p1", 
 	"down": "down_p1",
@@ -33,11 +41,12 @@ var character_input = [{
 ]
 var actions = ['left', 'right', 'up', 'down']
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	for player in range(4):
 		for action in actions:
 			if Input.is_action_just_pressed(character_input[player][action]):
 				Globals.players[player] = !Globals.players[player]
+				apply_shake()
 				if Globals.players[player]:
 					Globals.numPlayers += 1
 				else:
@@ -55,3 +64,11 @@ func _process(_delta: float) -> void:
 			get_tree().change_scene_to_file("res://scenes/catfight.tscn")
 	else:
 		press_play.text = "Press directional input to join!"
+		
+	if shake_strength > 0:
+		shake_strength = lerpf(shake_strength, 0, shakeFade * delta)
+		
+		camera_2d.offset = randomOffset()
+		
+func randomOffset():
+	return Vector2(rng.randf_range(-shake_strength,shake_strength),rng.randf_range(-shake_strength,shake_strength))
